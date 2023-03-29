d1eae8e47f078d19117fb0516472ddd31fb8f8c9
k1000pro <70303778+k1000pro@users.noreply.github.com>
2023-03-29 00:43:21 -0600

    Se inicializo el proyecto, se creo la base de datos, se crearon las entidades con persistencia, se creo el servidor local de payara, se creo el pool connection, se agrego el archivo de ComercioIT, RestResourcePattern y DeliveryDDL, y se comenzo el codigo para el lanzamiento de los testcontainers

---
+++ src/main/java/tpi135_2023/ingenieria/occ/ues/edu/sv/Delivery/entity/TipoProductoTipoComercioPK.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tpi135_2023.ingenieria.occ.ues.edu.sv.Delivery.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+/**
+ *
+ * @author Usuario
+ */
+@Embeddable
+public class TipoProductoTipoComercioPK implements Serializable {
+
+    @Basic(optional = false)
+    @Column(name = "id_tipo_producto")
+    private int idTipoProducto;
+    @Basic(optional = false)
+    @Column(name = "id_tipo_comercio")
+    private int idTipoComercio;
+
+    public TipoProductoTipoComercioPK() {
+    }
+
+    public TipoProductoTipoComercioPK(int idTipoProducto, int idTipoComercio) {
+        this.idTipoProducto = idTipoProducto;
+        this.idTipoComercio = idTipoComercio;
+    }
+
+    public int getIdTipoProducto() {
+        return idTipoProducto;
+    }
+
+    public void setIdTipoProducto(int idTipoProducto) {
+        this.idTipoProducto = idTipoProducto;
+    }
+
+    public int getIdTipoComercio() {
+        return idTipoComercio;
+    }
+
+    public void setIdTipoComercio(int idTipoComercio) {
+        this.idTipoComercio = idTipoComercio;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (int) idTipoProducto;
+        hash += (int) idTipoComercio;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof TipoProductoTipoComercioPK)) {
+            return false;
+        }
+        TipoProductoTipoComercioPK other = (TipoProductoTipoComercioPK) object;
+        if (this.idTipoProducto != other.idTipoProducto) {
+            return false;
+        }
+        if (this.idTipoComercio != other.idTipoComercio) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "tpi135_2023.ingenieria.occ.ues.edu.sv.Delivery.entity.TipoProductoTipoComercioPK[ idTipoProducto=" + idTipoProducto + ", idTipoComercio=" + idTipoComercio + " ]";
+    }
+    
+}
---
+++ src/test/java/tpi135_2023/ingenieria/occ/ues/edu/sv/Delivery/boundary/RestResourcePattern.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tpi135_2023.ingenieria.occ.ues.edu.sv.Delivery.boundary;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author jcpenya
+ */
+public class RestResourcePattern implements Serializable {
+
+    public final static String CONTAR_REGISTROS = "cantidad-registros";
+    public final static String PARAMETROS_FALTANTES = "missing-param";
+    public final static String ID_NOT_FOUND = "id-not-found";
+    public final static String NULL_PARAMETER = "null-parameter";
+    public final static String WRONG_PARAMETER = "wrong-parameter";
+
+}
